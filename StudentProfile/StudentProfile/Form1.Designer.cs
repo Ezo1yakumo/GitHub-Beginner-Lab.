5af884dbc3dcdbbef186343c5ff535eecce26bae
Abelon Tomaquin <abedelaria08@gmail.com>
2026-09-13 15:07:37 +0800

    Add StudentProfile Visual Studio project

---
+++ StudentProfile/StudentProfile/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace StudentProfile
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblStudProfile = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblStudProfile
+            // 
+            this.lblStudProfile.AutoSize = true;
+            this.lblStudProfile.Location = new System.Drawing.Point(42, 55);
+            this.lblStudProfile.Name = "lblStudProfile";
+            this.lblStudProfile.Size = new System.Drawing.Size(165, 13);
+            this.lblStudProfile.TabIndex = 0;
+            this.lblStudProfile.Text = "Student Profile: Abelon Tomaquin";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(251, 124);
+            this.Controls.Add(this.lblStudProfile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblStudProfile;
+    }
+}
+
