9c65579ac0172291c625154201a1d2315478d513
Abelon Tomaquin <abedelaria08@gmail.com>
2026-09-14 15:22:29 +0800

    Implement student email (#03)

--- StudentProfile/StudentProfile/Form1.Designer.cs
+++ StudentProfile/StudentProfile/Form1.Designer.cs
@@ -31,12 +31,13 @@
             this.lblStudProfile = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lblStudProfile
             // 
             this.lblStudProfile.AutoSize = true;
-            this.lblStudProfile.Location = new System.Drawing.Point(29, 42);
+            this.lblStudProfile.Location = new System.Drawing.Point(27, 19);
             this.lblStudProfile.Name = "lblStudProfile";
             this.lblStudProfile.Size = new System.Drawing.Size(187, 13);
             this.lblStudProfile.TabIndex = 0;
@@ -45,7 +46,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(42, 67);
+            this.label1.Location = new System.Drawing.Point(40, 44);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(156, 13);
             this.label1.TabIndex = 1;
@@ -54,17 +55,27 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(64, 90);
+            this.label2.Location = new System.Drawing.Point(57, 89);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(108, 13);
+            this.label2.Size = new System.Drawing.Size(157, 13);
             this.label2.TabIndex = 2;
-            this.label2.Text = "Course: Event Driven";
+            this.label2.Text = "Email: abedelaria08@gmail.com";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(71, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Course: Event Driven";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(251, 124);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.lblStudProfile);
@@ -81,6 +92,7 @@
         private System.Windows.Forms.Label lblStudProfile;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
